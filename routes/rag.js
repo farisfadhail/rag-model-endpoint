6210a5bac48a89dcbcd4651a70f5bb3b01c461d4
Faris Fadhail <farisfadhail@gmail.com>
2025-04-24 18:01:12 +0700

    add: index endpoint

--- routes/rag.js
+++ routes/rag.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { ingestDocument, generateAnswer, retrieveContext } = require("../controllers/ragController");
 
+router.get("/", (req, res) => {
+	res.json({ message: "RAG API is running" });
+});
+
 router.post("/ingest", ingestDocument);
 router.post("/generate", generateAnswer);
 router.post("/retrieve", retrieveContext);
