b375015f5b0d9720b6c2c32a147fb9f773342fd8
Faris Fadhail <farisfadhail@gmail.com>
2025-04-24 18:03:10 +0700

    update: index endpoin places

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+	res.json({ message: "RAG API is running" });
+});
+
 const ragRoutes = require("./routes/rag");
 app.use("/api/rag", ragRoutes);
 
--- routes/rag.js
+++ routes/rag.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { ingestDocument, generateAnswer, retrieveContext } = require("../controllers/ragController");
 
-router.get("/", (req, res) => {
-	res.json({ message: "RAG API is running" });
-});
-
 router.post("/ingest", ingestDocument);
 router.post("/generate", generateAnswer);
 router.post("/retrieve", retrieveContext);
